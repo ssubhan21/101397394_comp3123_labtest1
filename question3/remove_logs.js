89dc7907b685b70d56b05f11af2292329fa0420d
ssubhan21 <mohdabdulsubhan3@gmail.com>
2024-10-03 14:26:48 -0400

    lab test 1

--- question3/remove_logs.js
+++ question3/remove_logs.js
@@ -1,26 +1,17 @@
-
-// remove.js
 const fs = require('fs');
 const path = require('path');
 
-// Define the Logs directory path
 const logsDir = path.join(process.cwd(), 'Logs');
 
-// Function to remove log files and the directory
 function removeLogFiles() {
     if (fs.existsSync(logsDir)) {
-        // Read all files in the directory
         const files = fs.readdirSync(logsDir);
-        
-        // Output the file names to delete
         console.log('Files to delete:');
         files.forEach(file => {
             console.log(file);
-            // Remove each file
             fs.unlinkSync(path.join(logsDir, file));
         });
         
-        // Remove the Logs directory
         fs.rmdirSync(logsDir);
         console.log('Logs directory removed.');
     } else {
@@ -28,5 +19,5 @@ function removeLogFiles() {
     }
 }
 
-// Execute the function
+
 removeLogFiles();
